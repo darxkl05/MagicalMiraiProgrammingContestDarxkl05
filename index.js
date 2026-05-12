1d6cd08462c27d20b2c69e20961e889fd6c6af7e
linuxgabrielnorte <gabriel.norte01@estudiant.upf.edu>
2026-05-12 23:33:29 +0200

    Added text alive + Docs dev registration code

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-import { Player } from "textalive-app-api"; // Import PLayer class
+// Import player class from API
+import { Player } from "textalive-app-api";
 
-//Player.call(Player);
+// 単語が発声されていたら #text に表示する
+const animateWord = function (now, unit) {
+  if (unit.contains(now)) {
+    document.querySelector("#text").textContent = unit.text;
+  }
+};
 
-console.log("Miku");
+// TextAlive Player を作る
+const player = new Player({ app: { token: "your token" } });
+player.addListener({
+  // 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
+  onVideoReady: (v) => {
+    // 定期的に呼ばれる各単語の "animate" 関数をセットする
+    let w = player.video.firstWord;
+    while (w) {
+      w.animate = animateWord;
+      w = w.next;
+    }
+  },
+});
